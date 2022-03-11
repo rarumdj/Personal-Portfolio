f4e9390c11226070edec13b33635e450b5fefed6
David Johnson <davemoses3@gmail.com>
2022-03-11 17:24:55 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Work from "./containers/Work";
 function App() {
   return (
     <div className="App">
-      <section className="xl:h-full lg:h-screen md:h-full sm:h-screen section-gradient">
+      <section className="xl:h-full lg:h-screen sm:h-full h-screen section-gradient">
         <Header />
       </section>
       <About />
